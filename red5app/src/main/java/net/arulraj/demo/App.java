6a6817961f7dfbadcdb7d082274f2a3054124b33
Arulraj Venni <arulraj.venni@tatacommunications.com>
2013-03-22 11:45:43 +0530

    Support for red5-1.0.0

--- red5app/src/main/java/net/arulraj/demo/App.java
+++ red5app/src/main/java/net/arulraj/demo/App.java
@@ -1,13 +1,42 @@
 package net.arulraj.demo;
 
+
+import org.red5.server.adapter.ApplicationAdapter;
+import org.red5.server.api.IConnection;
+import org.red5.server.api.scope.IScope;
+import org.red5.server.api.stream.IServerStream;
+
 /**
- * Hello world!
+ * @author Arul
  *
  */
-public class App 
+public class App extends ApplicationAdapter
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+  private IScope appScope;
+
+  private IServerStream serverStream;
+
+  @Override
+  public boolean appStart(IScope app) {
+    super.appStart(app);
+    log.info("red5app appStart");
+    System.out.println("red5app appStart");
+    appScope = app;
+    return true;
+  }
+
+  @Override
+  public boolean appConnect(IConnection conn, Object[] params) {
+    log.info("red5app appConnect");
+    return super.appConnect(conn, params);
+  }
+
+  @Override
+  public void appDisconnect(IConnection conn) {
+    log.info("red5app appDisconnect");
+    if (appScope == conn.getScope() && serverStream != null) {
+      serverStream.close();
     }
+    super.appDisconnect(conn);
+  }
 }
